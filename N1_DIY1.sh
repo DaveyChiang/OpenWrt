41fa97aad226aac667a9d78d091327e4d10031fd
DaveyChiang <104084231+DaveyChiang@users.noreply.github.com>
2022-04-28 19:58:15 +0800

    Delete N1_DIY1.sh

--- N1_DIY1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# File name: N1_DIY1.sh
-# Description: Before Update feeds
-#
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
