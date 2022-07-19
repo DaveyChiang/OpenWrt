90b1209c72b08d51950abc0e04f8dd31b72f1d0f
DaveyChiang <104084231+DaveyChiang@users.noreply.github.com>
2022-07-19 20:02:34 +0800

    Update x86_64_DIY1.sh

--- x86_64_DIY1.sh
+++ x86_64_DIY1.sh
@@ -7,3 +7,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
