c8ff6890307a72c09f1e665de42e90ef115f478f
DaveyChiang <104084231+DaveyChiang@users.noreply.github.com>
2022-07-19 19:56:48 +0800

    Update x86_64_DIY2.sh

--- x86_64_DIY2.sh
+++ x86_64_DIY2.sh
@@ -14,7 +14,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='DaveyChiang'' p
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # git lua-maxminddb 依赖
-# git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 # Hello World
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
