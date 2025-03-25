09b0e322be207b51a377f71ed457a8711f35773e
ZqinKing <zqinking23@gmail.com>
2025-03-25 00:40:21 +0000

    移除miniupnp补丁。上游已增加强制转发功能

--- update.sh
+++ update.sh
@@ -679,7 +679,7 @@ main() {
     remove_unwanted_packages
     update_homeproxy
     fix_default_set
-    fix_miniupmpd
+    # fix_miniupmpd
     update_golang
     change_dnsmasq2full
     chk_fullconenat
