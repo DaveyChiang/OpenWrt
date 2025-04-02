1f92157c21a58c41cf4d643d065b44ea1348cb4d
ZqinKing <zqinking23@gmail.com>
2025-04-02 00:43:32 +0000

    passwall默认清空chnlist

--- update.sh
+++ update.sh
@@ -399,10 +399,15 @@ EOF
 }
 
 update_pw() {
-    local smartdns_lua_path="$BUILD_DIR/feeds/small8/luci-app-passwall/root/usr/share/passwall/helper_smartdns_add.lua"
+    local pw_share_dir="$BUILD_DIR/feeds/small8/luci-app-passwall/root/usr/share/passwall"
+    local smartdns_lua_path="$pw_share_dir/helper_smartdns_add.lua"
+    local rules_dir="$pw_share_dir/rules"
 
     # 删除 helper_smartdns_add.lua 文件中的特定行
     [ -f "$smartdns_lua_path" ] && sed -i '/force-qtype-SOA 65/d' "$smartdns_lua_path"
+
+    # 清空chnlist
+    [ -f "$rules_dir/chnlist" ] && echo "" >"$rules_dir/chnlist"
 }
 
 install_opkg_distfeeds() {
