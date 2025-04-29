2c28c72bac57bd2d51963caa5f6cfb3bc4a5fd7b
ZqinKing <zqinking23@gmail.com>
2025-04-29 08:07:32 +0000

    移除quectel_QMI_WWAN，编译失败，IPQ50XX/60XX可能不支持

--- update.sh
+++ update.sh
@@ -120,18 +120,19 @@ remove_unwanted_packages() {
         \rm -rf ./package/istore
     fi
 
-    if grep -q "nss_packages" "$BUILD_DIR/$FEEDS_CONF"; then
-        local nss_packages_dirs=(
-            "$BUILD_DIR/feeds/luci/protocols/luci-proto-quectel"
-            "$BUILD_DIR/feeds/packages/net/quectel-cm"
-            "$BUILD_DIR/feeds/packages/kernel/quectel-qmi-wwan"
-        )
-        for dir in "${nss_packages_dirs[@]}"; do
-            if [[ -d "$dir" ]]; then
-                \rm -rf "$dir"
-            fi
-        done
-    fi
+    # ipq60xx/50xx不支持NSS offload mnet_rx
+    #if grep -q "nss_packages" "$BUILD_DIR/$FEEDS_CONF"; then
+    #    local nss_packages_dirs=(
+    #        "$BUILD_DIR/feeds/luci/protocols/luci-proto-quectel"
+    #        "$BUILD_DIR/feeds/packages/net/quectel-cm"
+    #        "$BUILD_DIR/feeds/packages/kernel/quectel-qmi-wwan"
+    #    )
+    #    for dir in "${nss_packages_dirs[@]}"; do
+    #        if [[ -d "$dir" ]]; then
+    #            \rm -rf "$dir"
+    #        fi
+    #    done
+    #fi
 
     # 临时放一下，清理脚本
     if [ -d "$BUILD_DIR/target/linux/qualcommax/base-files/etc/uci-defaults" ]; then
