358c00ab04b638f1f76bf8697f057e0eac9c1520
ZqinKing <zqinking23@gmail.com>
2025-05-20 08:41:13 +0000

    easytier编译异常，暂时取消。（上游问题）

--- update.sh
+++ update.sh
@@ -148,7 +148,8 @@ install_small8() {
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
         luci-theme-argon netdata luci-app-netdata lucky luci-app-lucky luci-app-openclash luci-app-homeproxy \
         luci-app-amlogic nikki luci-app-nikki tailscale luci-app-tailscale oaf open-app-filter luci-app-oaf \
-        easytier luci-app-easytier msd_lite luci-app-msd_lite
+        msd_lite luci-app-msd_lite
+        #easytier luci-app-easytier
 }
 
 install_feeds() {
@@ -788,7 +789,7 @@ main() {
     install_feeds
     support_fw4_adg
     update_script_priority
-    fix_easytier
+    # fix_easytier
     update_geoip
     update_package "xray-core"
     # update_proxy_app_menu_location
